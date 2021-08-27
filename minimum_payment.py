7ab74a7da8e39080fe08af1bec4d9f80448aad8f
Diego-Aero <85786727+Diego-Aero@users.noreply.github.com>
2021-08-27 16:45:45 +0200

    Obtener balance cero
    Para calcular el valor de minimumPayment que proporciona balance cero al final del año necesitamos algo más elaborado. Establecemos un límite inferior y un límite superior y a partir de ellos vamos iterando mediante bisección hasta obtener el resultado final con un margen de error epsilon.

--- minimum_payment.py
+++ minimum_payment.py
@@ -21,3 +21,27 @@ while balance:
         minimumPayment+=10
 
 print('Lowest payment:', minimumPayment)
+
+annualInterestRate=0.2
+balance=3926
+balanceInitial=balance
+high=balance*(1+annualInterestRate/12)**12/12
+#Un buen valor inicial es la paga del tiron con todos los intereses acumulados en el ultimo mes
+#Un buen límite superior para el pago mensual sería una doceava parte del saldo, después de tener sus intereses compuestos mensualmente durante todo un año.
+low=balance/12
+#Un buen valor inicial es la doceaba parte puesto que si no hay intereses
+#la deuda puede pagarse asi en los doce meses
+epsilon=0.01
+while balance:
+    balance=balanceInitial
+    minimumPayment=(high+low)/2
+    balance=PayingDebt(minimumPayment, balance, annualInterestRate)
+    if abs(balance)<=epsilon:
+        break
+    else:
+        if balance>0:
+            low=minimumPayment
+        elif balance<0:
+            high=minimumPayment
+
+print('Lowest payment:', round(minimumPayment, 2))
